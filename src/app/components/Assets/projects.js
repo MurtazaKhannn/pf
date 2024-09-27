3f242e1f41aef05c94bd348da1f0b35b5cef3491
MurtazaKhannn <152141459+MurtazaKhannn@users.noreply.github.com>
2024-09-27 22:30:16 +0530

    commmit projects

--- src/app/components/Assets/projects.js
+++ src/app/components/Assets/projects.js
@@ -1,4 +1,25 @@
 let projects = [
+
+  {
+    name: "Indian Oil",
+    video: "/videos/ioclf.mp4", // URL path relative to the public folder
+    description: "Developed a comprehensive Tender Document Management System for Indian Oil, utilizing the MERN stack (MongoDB, Express.js, React, Node.js). This web application is designed to streamline the management and processing of tender documents, enhancing efficiency and organization in handling large volumes of critical documents.",
+    images: ["image1.jpg", "image2.jpg"],
+    link: "https://iocl-project-frontend.vercel.app/",
+    github: "https://github.com/MurtazaKhannn/IOCL-ProjectFrontend",
+    tags: ["Management system", "Efficient"],
+    stack: ["Mongo", "Express", "React", "Node"]
+  },
+  {
+    name: "hentAI (In Progress)",
+    video: "/videos/hentAI.mp4", // URL path relative to the public folder
+    description: "Introducing hentAi – a chatbot I've developed using Next.js, PostgreSQL, Prisma, and GeminiAPI. It currently features real-time AI conversations and document processing capabilities. More features will be added soon as I continue to enhance its functionality! Stay tuned for more updates as hentAi evolves into an even more powerful tool. ",
+    images: ["image1.jpg", "image2.jpg"],
+    link: "https://github.com/MurtazaKhannn/hai",
+    github: "https://github.com/MurtazaKhannn/hai",
+    tags: ["ChatBot", "Efficient and Interactive"],
+    stack: ["Postgres", "Nextjs", "prisma", "neonDb"]
+  },
   {
     name: "Silva Nest",
     video: "/videos/snpf.mp4", // URL path relative to the public folder
@@ -10,14 +31,34 @@ let projects = [
     stack: ["Mongo", "Express", "React", "Node"]
   },
   {
-    name: "Indian Oil",
-    video: "/videos/ioclf.mp4", // URL path relative to the public folder
-    description: "Developed a comprehensive Tender Document Management System for Indian Oil, utilizing the MERN stack (MongoDB, Express.js, React, Node.js). This web application is designed to streamline the management and processing of tender documents, enhancing efficiency and organization in handling large volumes of critical documents.",
+    name: "Poetry (In Progress)",
+    video: "/videos/poet.mp4", // URL path relative to the public folder
+    description: "Welcome to My Poetry Haven – a creative project currently in development, built with Next.js, MongoDB, GSAP, and Framer Motion. This platform aims to bring poetry to life through captivating animations and dynamic visuals. While I'm still fine-tuning features and enhancing the user experience, I invite you to explore the journey as I craft a unique space for poetry enthusiasts. Stay tuned for updates and new poetic creations!",
     images: ["image1.jpg", "image2.jpg"],
-    link: "https://iocl-project-frontend.vercel.app/",
-    github: "https://github.com/MurtazaKhannn/IOCL-ProjectFrontend",
-    tags: ["Managemnt system", "efficient"],
-    stack: ["Mongo", "Express", "React", "Node"]
+    link: "https://github.com/MurtazaKhannn/pOETs",
+    github: "https://github.com/MurtazaKhannn/pOETs",
+    tags: ["Poetry exp", "Beautiful"],
+    stack: ["Mongo", "Nextjs", "GSAP", "Framer Motion"]
+  },
+  {
+    name: "Talk (In Progress)",
+    video: "/videos/talk.mp4", // URL path relative to the public folder
+    description: "Introducing Talk – a social media app currently in the works, built on the MERN stack. With a user-friendly interface, Talk allows you to do everything you'd expect from a social media platform: tweet, post, share, and connect with others. Designed to be visually pleasing and intuitive, Talk is your space to express yourself and engage with a vibrant community. Stay tuned for more features as I continue to develop this exciting project!",
+    images: ["image1.jpg", "image2.jpg"],
+    link: "https://github.com/MurtazaKhannn/talkFrontend",
+    github: "https://github.com/MurtazaKhannn/talkFrontend",
+    tags: ["social media", "Interactive"],
+    stack: ["Mongo", "Express", "GSAP", "React"]
+  },
+  {
+    name: "Chess (In Progress)",
+    video: "/videos/chess.mp4", // URL path relative to the public folder
+    description: "Introducing My Chess App – currently in progress, this app allows you to play chess and make every move smoothly. Built using Socket.IO, EJS, Express, and Chess.js, it provides a seamless gaming experience. I'm working hard to add more features soon, making it an even more engaging platform for chess enthusiasts. Stay tuned for updates!",
+    images: ["image1.jpg", "image2.jpg"],
+    link: "https://github.com/MurtazaKhannn/chess",
+    github: "https://github.com/MurtazaKhannn/chess",
+    tags: ["Chess", "Smooth"],
+    stack: ["socket.io", "ejs", "express", "chessjs"]
   },
 ];
 
